4bd17b9d44880df2196451c2355dbcd8fd818f3a
kade <wangkai@youxin.cloud>
2022-07-29 19:09:25 +0800

    basic config

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import count from "./js/count";
+import sum from "./js/sum";
+import "./css/index.css";
+import "./css/iconfont.css";
+import "./less/index.less";
+import "./scss/index.sass";
+import "./scss/index.scss";
+import "./styl/index.styl";
+
+let res1 = count(2,1);
+let res2 = sum(1,2,3,4);
+console.log(res1);
+console.log(res2);
