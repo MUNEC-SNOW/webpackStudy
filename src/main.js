77ddd25659082d5e966420164f14c7a292d69c79
kade <wangkai@youxin.cloud>
2022-08-01 20:25:09 +0800

    webpack basic done

--- src/main.js
+++ src/main.js
@@ -11,3 +11,15 @@ let res1 = count(2,1);
 let res2 = sum(1,2,3,4);
 console.log(res1);
 console.log(res2);
+
+if (module.hot) {
+    module.hot.accept("./js/count.js", function (count) {
+      const result1 = count(2, 1);
+      console.log(result1);
+    });
+  
+    module.hot.accept("./js/sum.js", function (sum) {
+      const result2 = sum(1, 2, 3, 4);
+      console.log(result2);
+    });
+  }
